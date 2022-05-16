4f7430894cb2fda89196b83a0907ca599a1a09ab
JeffersonPulido <jeffersonpulido55@gmail.com>
2022-05-16 08:34:20 -0500

    v1.1

--- assets/views/home.php
+++ assets/views/home.php
@@ -4,14 +4,53 @@
         <!--====META=====--> 
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!--====ICO=====--> 
-        <link rel="shortcut icon" href="assets/img/icono.ico"> 
+        <link rel="shortcut icon" href="../img/icono.ico"> 
         <!--====CSS=====-->
         <!--=====LIBS====--> 
         <script src="https://kit.fontawesome.com/c159e6bc0d.js" crossorigin="anonymous"></script>
         <!--=====TITLE====--> 
-        <title>Home | Alejandra Rodriguez SAS</title>
+        <title>Home | Alejandra Rodríguez S.A.S</title>
+        <!--=====LIBS====--> 
+        <link rel="stylesheet" type="text/css" href="../libs/font-awesome/css/all.css" />
+        <link rel="stylesheet" type="text/css" href="../libs/bootstrap-4.5.0/css/bootstrap.min.css" id="bootstrap-css">
+        <link rel="stylesheet" type="text/css" href="../libs/DataTables/datatables.min.css" />
+        <script type="text/javascript" src="../libs/jquery-v3.6.0/jquery.min.js"></script>
+        <script type="text/javascript" src="../libs/popperjs/popper.min.js"></script>
+        <script type="text/javascript" src="../libs/bootstrap-4.5.0/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="../libs/DataTables/datatables.min.js"></script>
+        <script type="text/javascript" src="../libs/DataTables/JSZip-2.5.0/jszip.min.js"></script>
+        <script type="text/javascript" src="../libs/DataTables/pdfmake-0.1.36/pdfmake.min.js"></script>
+        <script type="text/javascript" src="../libs/DataTables/pdfmake-0.1.36/vfs_fonts.js"></script>
+        <script type="text/javascript" src="../libs/DataTables/Buttons-1.7.1/js/buttons.html5.min.js"></script>
+        <script type="text/javascript" src="../libs/DataTables/Buttons-1.7.1/js/buttons.print.min.js"></script>
+        <script type="text/javascript" src="../libs/font-awesome/js/all.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
-        <h2>hola</h2>
+        <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="">Alejandra Rodríguez S.A.S</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">PLANES Y PRECIOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">INTERESADOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">CONSULTAS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">CERRAR SESION</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </body>
 </html>
