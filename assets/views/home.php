3304fa0ba807b2b3b1531844c3d498223697d6bb
JeffersonPulido <jeffersonpulido55@gmail.com>
2022-05-16 10:20:43 -0500

    v1.2

--- assets/views/home.php
+++ assets/views/home.php
@@ -1,29 +1,30 @@
 <!--LANDING PAGE GENERAL-->
 <html>
     <head>
-        <!--====META=====--> 
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!--====ICO=====--> 
-        <link rel="shortcut icon" href="../img/icono.ico"> 
-        <!--====CSS=====-->
-        <!--=====LIBS====--> 
+        <!--====META===-->
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!--===TITLE====-->
+        <title>Sign In | Alejandra Rodríguez S.A.S</title>
+        <!--====ICO===-->
+        <link rel="shortcut icon" href="assets/img/icono.ico"> 
+        <!--====CSS===-->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <!--====LIBS===-->
         <script src="https://kit.fontawesome.com/c159e6bc0d.js" crossorigin="anonymous"></script>
-        <!--=====TITLE====--> 
-        <title>Home | Alejandra Rodríguez S.A.S</title>
-        <!--=====LIBS====--> 
-        <link rel="stylesheet" type="text/css" href="../libs/font-awesome/css/all.css" />
-        <link rel="stylesheet" type="text/css" href="../libs/bootstrap-4.5.0/css/bootstrap.min.css" id="bootstrap-css">
-        <link rel="stylesheet" type="text/css" href="../libs/DataTables/datatables.min.css" />
-        <script type="text/javascript" src="../libs/jquery-v3.6.0/jquery.min.js"></script>
-        <script type="text/javascript" src="../libs/popperjs/popper.min.js"></script>
-        <script type="text/javascript" src="../libs/bootstrap-4.5.0/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="../libs/DataTables/datatables.min.js"></script>
-        <script type="text/javascript" src="../libs/DataTables/JSZip-2.5.0/jszip.min.js"></script>
-        <script type="text/javascript" src="../libs/DataTables/pdfmake-0.1.36/pdfmake.min.js"></script>
-        <script type="text/javascript" src="../libs/DataTables/pdfmake-0.1.36/vfs_fonts.js"></script>
-        <script type="text/javascript" src="../libs/DataTables/Buttons-1.7.1/js/buttons.html5.min.js"></script>
-        <script type="text/javascript" src="../libs/DataTables/Buttons-1.7.1/js/buttons.print.min.js"></script>
-        <script type="text/javascript" src="../libs/font-awesome/js/all.js"></script>
+        <link rel="stylesheet" type="text/css" href="assets/libs/font-awesome/css/all.css" />
+        <link rel="stylesheet" type="text/css" href="assets/libs/bootstrap-4.5.0/css/bootstrap.min.css" id="bootstrap-css">
+        <link rel="stylesheet" type="text/css" href="assets/libs/DataTables/datatables.min.css" />
+        <script type="text/javascript" src="assets/libs/jquery-v3.6.0/jquery.min.js"></script>
+        <script type="text/javascript" src="assets/libs/popperjs/popper.min.js"></script>
+        <script type="text/javascript" src="assets/libs/bootstrap-4.5.0/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="assets/libs/DataTables/datatables.min.js"></script>
+        <script type="text/javascript" src="assets/libs/DataTables/JSZip-2.5.0/jszip.min.js"></script>
+        <script type="text/javascript" src="assets/libs/DataTables/pdfmake-0.1.36/pdfmake.min.js"></script>
+        <script type="text/javascript" src="assets/libs/DataTables/pdfmake-0.1.36/vfs_fonts.js"></script>
+        <script type="text/javascript" src="assets/libs/DataTables/Buttons-1.7.1/js/buttons.html5.min.js"></script>
+        <script type="text/javascript" src="assets/libs/DataTables/Buttons-1.7.1/js/buttons.print.min.js"></script>
+        <script type="text/javascript" src="assets/libs/font-awesome/js/all.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
@@ -31,24 +32,24 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-warning">
             <div class="container-fluid">
                 <a class="navbar-brand" href="">Alejandra Rodríguez S.A.S</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">PLANES Y PRECIOS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">INTERESADOS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">CONSULTAS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">CERRAR SESION</a>
-                        </li>
-                    </ul>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">PLANES Y PRECIOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">INTERESADOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CONSULTAS</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <a class="btn btn-outline-success" href="../../index.html">SALIR</a>
+                </form>
                 </div>
             </div>
         </nav>
