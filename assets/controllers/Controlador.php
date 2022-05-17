48e59507268c8cf1d1d19a61e62170f85d2fbe56
JeffersonPulido <jeffersonpulido55@gmail.com>
2022-05-17 16:56:23 -0500

    Modulo2 Controlador

--- assets/controllers/Controlador.php
+++ assets/controllers/Controlador.php
@@ -51,39 +51,43 @@ class ControlU
     public function __construct()
 
     {
-        $this->MODEL = new UsuarioInt();
+        $this->MODEL = new Usuario();
     }
     public function index()
     {
-        include_once '../views/inicio.php';
+        include_once '../views/inicioU.php';
     }
     public function nuevo()
     {
-        $guar = new UsuarioInt();
+        $guar = new Usuario();
         if (isset($_REQUEST['Id_Usuario'])) {
             $guar = $this->MODEL->cargarPersonxID($_REQUEST['Id_Usuario']);
         }
-        include_once '../views/save.php';
+        include_once '../views/saveU.php';
     }
 
     public function guardar()
     {
 
-        $guar = new UsuarioInt();
+        $guar = new Usuario();
         $guar->Id_Usuario = $_POST['txtId_Usuario'];
         $guar->Nombre_Usuario = $_POST['txtNombre'];
         $guar->Cedula_Usuario = $_POST['txtCedula'];
         $guar->Correo_Usuario = $_POST['txtCorreo'];
         $guar->Num_Usuario = $_POST['txtCelular'];
+        $guar->Dire_Usuario = $_POST['txtDireccion'];
+        $guar->Eps_Usuario = $_POST['txtEps'];
+        $guar->Pensiones_Usuario = $_POST['txtPensiones'];
+        $guar->Plan_Usuario = $_POST['txtPlan'];
         $guar->Observacion = $_POST['txtObservacion'];
         $guar->Id_Usuario > 0 ? $this->MODEL->actualizarDatos($guar) : $this->MODEL->insertar($guar);
-        header("location: ../views/init.php"); 
+        header("location: ../views/initU.php"); 
     }
 
     public function eliminar()
     {
         $this->MODEL->delete($_REQUEST['Id_Usuario']);
-        header("location: ../views/init.php"); 
+        header("location: ../views/initU.php"); 
     }
 }
 
